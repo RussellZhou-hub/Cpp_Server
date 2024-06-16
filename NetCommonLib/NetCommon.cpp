5873baaea81df1ae21f6b8fb9c325d95ded8d624
RussellZhou-hub <938311288@qq.com>
2024-06-17 04:43:22 +0800

    add source file

---
+++ NetCommonLib/NetCommon.cpp
@@ -0,0 +1,11 @@
+// NetCommon.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "net_common.h"
+
+// TODO: This is an example of a library function
+void fnNetCommon()
+{
+}
---
+++ NetCommonLib/net_common.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <optional>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+
+#ifdef _WIN32
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+#endif
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
